9737b86ea500c8bdf0a832533eba2e937325a7ac
jjgh <jjgh@users.noreply.github.com>
2013-10-23 22:14:31 +0200

    Update jruby awt.rb

--- jruby awt.rb
+++ jruby awt.rb
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 # code is not recent (about 2 ys ago), beware!!
 
-=======
->>>>>>> e9d18aece80ba9ec5f93d3e5d319c0f2645ed774
 #La libreria Swing è un'estensione di AWT
 
 require 'java'
